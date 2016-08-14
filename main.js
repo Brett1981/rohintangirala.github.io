222741f102471e6578117ea961084b135b7152fc
Rohin Tangirala <rohinkt@gmail.com>
2016-08-14 15:48:16 -0700

    Updated scroll reveal and tweenlite

--- main.js
+++ main.js
@@ -17,8 +17,10 @@ $(window).load(function() {
   var title = document.getElementById("title");
   var tagline = document.getElementById("tagline");
   var findOutMore = document.getElementById("find-out-more");
-  TweenLite.from(title, 0.75, {delay:.05, opacity: 0});
-  TweenLite.from(tagline, 0.75, {delay:.75, opacity: 0});
-  TweenLite.from(findOutMore, 0.75, {delay: 1.25, opacity: 0});
-  window.sr = ScrollReveal({origin: 'left', distance: '100%'}).reveal('.scroll-item');
+  TweenLite.from(title, 0.75, {delay:.5, opacity: 0});
+  TweenLite.from(tagline, 0.75, {delay:1, opacity: 0});
+  TweenLite.from(findOutMore, 0.75, {delay: 1.5, opacity: 0});
+  window.sr = ScrollReveal();
+  sr.reveal('.scroll-item-left', {origin: 'left', distance: '100%'});
+  sr.reveal('.scroll-item-right', {origin: 'right', distance: '100%'});
 });
