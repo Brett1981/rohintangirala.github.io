a7e8de4671dd56b5382e0526451e198a9ee8a509
Rohin Tangirala <rohinkt@gmail.com>
2016-08-14 13:13:45 -0700

    Added GSAP

--- main.js
+++ main.js
@@ -14,5 +14,11 @@ $(function() {
 });
 
 $(window).load(function() {
+  var title = document.getElementById("title");
+  var tagline = document.getElementById("tagline");
+  var findOutMore = document.getElementById("find-out-more");
+  TweenLite.from(title, 0.75, {delay:.05, opacity: 0});
+  TweenLite.from(tagline, 0.75, {delay:.75, opacity: 0});
+  TweenLite.from(findOutMore, 0.75, {delay: 1.25, opacity: 0});
   window.sr = ScrollReveal({origin: 'left', distance: '100%'}).reveal('.scroll-item');
 });
